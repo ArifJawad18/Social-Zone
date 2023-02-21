75c9568f0f440d01f7b36a7a6ed7720d27812470
Arifjawad18 <arif.jawad18@gmail.com>
2023-02-22 04:46:25 +0600

    edit commit

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -1,26 +1,53 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const Checkout = () => {
+  const {register, handleSubmit, formState: {errors } } = useForm();
+  const handleAddDoctor = data => {
+    console.log(data);
+
+
+
+  }
+
   return (
-    <form>
-      <div className="flex justify-center">
-        <textarea
-          placeholder=" Textarea Field"
-          className="textarea textarea-bordered textarea-lg w-full max-w-xs mt-5 ml-5"
-        ></textarea>
-        <fieldset className="w-full space-y-1 text-gray-800">
-          <div className="flex">
+    <div className="h-[300px] flex justify-center items-center mt-5">
+     <form onSubmit={handleSubmit(handleAddDoctor)}>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
             <input
-              type="file"
-              name="files"
-              id="files"
-              className="px-8 py-12 border-2 border-dashed rounded-md border-gray-300 text-gray-600 bg-gray-100 mt-5 ml-5"
+              type="text"
+              {...register("name", { required: "Name is Empty" })}
+              className="input input-bordered  w-full max-w-xs"
             />
-            <button className="btn btn-outline mt-12 ml-5">Submit</button>
+            {errors.name && (
+              <p className="text-red-500">{errors.name?.message}</p>
+            )}
           </div>
-        </fieldset>
-      </div>
-    </form>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              {...register("email", { required: "Email is Empty" })}
+              className="input input-bordered  w-full max-w-xs"
+            />
+            {errors.email && (
+              <p className="text-red-500">{errors.email?.message}</p>
+            )}
+          </div>
+         
+          <input
+            className=" btn  w-full mt-5 px-8 py-3 font-semibold rounded-md bg-sky-600 text-gray-50"
+            value="Submit"
+            type="submit"
+          />
+        
+        </form>
+   </div>
   );
 };
 
