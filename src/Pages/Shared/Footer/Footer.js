2b860276a4daae6cf410e2061c4d53e5a9496c84
Arifjawad18 <arif.jawad18@gmail.com>
2023-04-08 02:59:08 +0600

    add navbar

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -3,11 +3,11 @@ import React from "react";
 const Footer = () => {
   return (
     <div>
-      <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded mt-5">
-        <div>
-          <p>Copyright © 2023 - All right reserved</p>
-        </div>
+      <footer>
+        
       </footer>
+       
+    
     </div>
   );
 };
